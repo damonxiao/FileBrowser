304cbd5943f909a2e76e5ad5e2d806727b0baf99
damonxiao <qukers@126.com>
2013-08-03 21:01:35 +0800

    do firstly job,code the file list.

--- src/com/ll/filebrowser/ui/FileListActivity.java
+++ src/com/ll/filebrowser/ui/FileListActivity.java
@@ -1,18 +1,23 @@
-
 package com.ll.filebrowser.ui;
 
 import android.annotation.TargetApi;
 import android.app.ActionBar;
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
 
 import com.ll.filebrowser.R;
 import com.ll.filebrowser.util.FileFinder;
+import com.ll.filebrowser.util.FileListAdapter;
 import com.ll.filebrowser.util.Logger;
 import com.ll.filebrowser.util.NavigatorAdapter;
 
@@ -20,88 +25,185 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
-public class FileListActivity extends Activity implements ActionBar.OnNavigationListener {
+public class FileListActivity extends Activity implements
+		ActionBar.OnNavigationListener, OnItemClickListener {
 
-    private static final String TAG = "FileListActivity";
+	private static final String TAG = "FileListActivity";
 
-    private List<File> mNavigatorDirs;
+	private List<File> mNavigatorDirs;
 
-    private NavigatorAdapter mNavigatorAdapter;
-    
-    private ListView mFileListView;
+	private NavigatorAdapter mNavigatorAdapter;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        prepareActionBar();
-    }
+	private ListView mFileListView;
 
-    private void prepareActionBar() {
-        mNavigatorDirs = new ArrayList<File>();
-        mNavigatorDirs.add(Environment.getRootDirectory());
-        mNavigatorDirs.add(Environment.getExternalStorageDirectory());
-        mNavigatorDirs.add(Environment
-                .getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS));
-        mNavigatorDirs.add(Environment
-                .getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM));
-        mNavigatorDirs.add(Environment
-                .getExternalStoragePublicDirectory(Environment.DIRECTORY_MOVIES));
-        mNavigatorDirs.add(Environment
-                .getExternalStoragePublicDirectory(Environment.DIRECTORY_MUSIC));
-        mNavigatorDirs.add(Environment
-                .getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES));
+	private FileListAdapter mFileListAdapter;
 
-        mNavigatorAdapter = new NavigatorAdapter(mNavigatorDirs, this);
+	private File mCurFolder;
 
-        ActionBar actionBar = getActionBar();
-        actionBar.setDisplayShowTitleEnabled(false);
-        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_LIST);
-        // List<File>
-        // Set up the dropdown list navigation in the action bar.
-        actionBar.setListNavigationCallbacks(
-                // Specify a SpinnerAdapter to populate the dropdown list.
-                mNavigatorAdapter,
-                this);
-    }
+	private FileFinder mExecutingFinder;
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		prepareActionBar();
+		mCurFolder = Environment.getRootDirectory();
+		mFileListView = (ListView) findViewById(R.id.file_list_view);
+		mFileListView.setOnItemClickListener(this);
+		loadFolder(mCurFolder);
+	}
 
-    /**
-     * Backward-compatible version of {@link ActionBar#getThemedContext()} that
-     * simply returns the {@link android.app.Activity} if
-     * <code>getThemedContext</code> is unavailable.
-     */
-    @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
-    private Context getActionBarThemedContextCompat() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
-            return getActionBar().getThemedContext();
-        } else {
-            return this;
-        }
-    }
+	private void loadFolder(File folder) {
+		if (folder == null || !folder.exists()) {
+			return;
+		}
+		if (folder.getParentFile() != null && folder.getParentFile().exists()) {
+			mNavigatorDirs.set(0, folder);
+			mNavigatorAdapter.notifyDataSetChanged();
+		} else {
+			mNavigatorDirs.set(0, Environment.getRootDirectory());
+			mNavigatorAdapter.notifyDataSetChanged();
+		}
+		if (mExecutingFinder != null) {
+			mExecutingFinder.cancel(true);
+		}
+		showProgressDialog();
+		mExecutingFinder = new FileFinder(new FileFinder.IFileFinderCallback() {
+			@Override
+			public void onLoadSuccess(List<File> files) {
+				dismissProgressDialog();
+				if (mFileListAdapter == null) {
+					mFileListAdapter = new FileListAdapter(
+							FileListActivity.this, files);
+					mFileListView.setAdapter(mFileListAdapter);
+				} else {
+					mFileListAdapter.refreshData(files);
+				}
+			}
 
-    @Override
-    public boolean onNavigationItemSelected(int itemPosition, long itemId) {
-        File selectedItem = mNavigatorAdapter.getItem(itemPosition);
-        Logger.d(TAG, "onNavigationItemSelected()[selectedItem:" + selectedItem + "]");
-        new FileFinder(new FileFinder.IFileFinderCallback() {
-            @Override
-            public void onLoadSuccess(List<File> files) {
-                
-            }
+			@Override
+			public void onLoadFailed(int resonCode) {
+				dismissProgressDialog();
+			}
+		});
+		mExecutingFinder.execute(folder);
+	}
 
-            @Override
-            public void onLoadFailed(int resonCode) {
+	private ProgressDialog mProgressDialog;
 
-            }
-        }).execute(selectedItem);
-        return true;
-    }
+	private void showProgressDialog() {
+		if (mProgressDialog == null) {
+			mProgressDialog = new ProgressDialog(this);
+			mProgressDialog.setCanceledOnTouchOutside(false);
+			mProgressDialog.setCancelable(false);
+		}
+		if (mProgressDialog.isShowing()) {
+			mProgressDialog.dismiss();
+		}
+		mProgressDialog.show();
+	}
+
+	private void dismissProgressDialog() {
+		if (mProgressDialog != null) {
+			mProgressDialog.dismiss();
+		}
+	}
+
+	private void prepareActionBar() {
+		mNavigatorDirs = new ArrayList<File>();
+		mNavigatorDirs.add(Environment.getRootDirectory());
+		mNavigatorDirs.add(Environment.getExternalStorageDirectory());
+		mNavigatorDirs
+				.add(Environment
+						.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS));
+		mNavigatorDirs.add(Environment
+				.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM));
+		mNavigatorDirs
+				.add(Environment
+						.getExternalStoragePublicDirectory(Environment.DIRECTORY_MOVIES));
+		mNavigatorDirs
+				.add(Environment
+						.getExternalStoragePublicDirectory(Environment.DIRECTORY_MUSIC));
+		mNavigatorDirs
+				.add(Environment
+						.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES));
+
+		mNavigatorAdapter = new NavigatorAdapter(mNavigatorDirs, this);
+
+		ActionBar actionBar = getActionBar();
+		actionBar.setDisplayShowTitleEnabled(false);
+		actionBar.setDisplayHomeAsUpEnabled(true);
+		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_LIST);
+		// List<File>
+		// Set up the dropdown list navigation in the action bar.
+		actionBar.setListNavigationCallbacks(
+		// Specify a SpinnerAdapter to populate the dropdown list.
+				mNavigatorAdapter, this);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	/**
+	 * Backward-compatible version of {@link ActionBar#getThemedContext()} that
+	 * simply returns the {@link android.app.Activity} if
+	 * <code>getThemedContext</code> is unavailable.
+	 */
+	@TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
+	private Context getActionBarThemedContextCompat() {
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
+			return getActionBar().getThemedContext();
+		} else {
+			return this;
+		}
+	}
+
+	@Override
+	public boolean onNavigationItemSelected(int itemPosition, long itemId) {
+		File selectedItem = mNavigatorAdapter.getItem(itemPosition);
+		Logger.d(TAG, "onNavigationItemSelected()[selectedItem:" + selectedItem
+				+ "]");
+		if (selectedItem != null && selectedItem.exists()
+				&& selectedItem.isDirectory()) {
+			mCurFolder = selectedItem;
+			loadFolder(mCurFolder);
+		}
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			Logger.d(TAG,
+					"onOptionsItemSelected()[click home go to parent folder]");
+			goToParent();
+			return true;
+		default:
+			break;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	private void goToParent() {
+		if (mCurFolder != null && mCurFolder.getParentFile() != null
+				&& mCurFolder.getParentFile().exists()) {
+			mCurFolder = mCurFolder.getParentFile();
+			loadFolder(mCurFolder);
+		}
+	}
+
+	@Override
+	public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+		File clickItem = mFileListAdapter.getItem(arg2);
+		if (clickItem != null && clickItem.exists() && clickItem.isDirectory()) {
+			mCurFolder = clickItem;
+			loadFolder(mCurFolder);
+		}
+	}
 
 }
--- src/com/ll/filebrowser/util/FileListAdapter.java
+++ src/com/ll/filebrowser/util/FileListAdapter.java
@@ -1,28 +1,41 @@
 
 package com.ll.filebrowser.util;
 
+import java.io.File;
+import java.util.List;
+
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-import java.io.File;
-import java.util.List;
+import com.ll.filebrowser.R;
 
 public class FileListAdapter extends BaseAdapter {
 
-    private Context mContext;
     private LayoutInflater mInflater;
     private List<File> mFileList;
 
+    public FileListAdapter(Context context,List<File> fileList){
+    	mInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    	mFileList = fileList;
+    }
+    
+    public void refreshData(List<File> files){
+    	mFileList = files;
+    	notifyDataSetChanged();
+    }
+    
     @Override
     public int getCount() {
         return mFileList == null ? 0 : mFileList.size();
     }
 
     @Override
-    public Object getItem(int position) {
+    public File getItem(int position) {
         return mFileList.get(position);
     }
 
@@ -33,8 +46,15 @@ public class FileListAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        // TODO Auto-generated method stub
-        return null;
+    	if(convertView == null){
+    		convertView = mInflater.inflate(R.layout.file_list_item, null);
+    	}
+    	File file = mFileList.get(position);
+    	ImageView fileTypeIcon = (ImageView) convertView.findViewById(R.id.file_type_icon);
+    	TextView fileName = (TextView) convertView.findViewById(R.id.file_name);
+    	fileName.setText(file.getName());
+    	TextView fileDesc = (TextView) convertView.findViewById(R.id.file_desc);
+        return convertView;
     }
 
 }
